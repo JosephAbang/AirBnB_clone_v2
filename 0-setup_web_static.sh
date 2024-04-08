34e35b66f16d68a94d5da3325e2e5cdeca2861d7
JosephAbang <abangjosef@gmail.com>
2024-04-08 12:45:05 +0100

    Modify permissions of file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,19 +6,19 @@ sudo apt-get update
 sudo apt-get -y install nginx
 
 # Create folders and file
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 echo "Hello World!" > /data/web_static/releases/test/index.html
 
 # Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership to user and group
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+chown -R ubuntu/data/
+chgrp -R ubuntu/data/
 
 sudo bash -c 'echo "server {
     listen 80 default_server;
