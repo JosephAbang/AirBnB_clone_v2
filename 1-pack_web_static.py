01aecdf41448854cdfe0794f117d6d70a627423d
JosephAbang <abangjosef@gmail.com>
2024-04-09 03:30:28 +0100

    Script creates tgz archive folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script generates a .tgz archive from the contents of the web_static folder"""
+
+from fabric.api import local, run
+from datetime import datetime
+
+def do_pack():
+    """
+        Creates a tgz archive of the specified directory on the local machine.
+    """
+    try:
+        run("mkdir -p versions")
+        date_now =  datetime.now()
+        archive_path = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+                date_now.year,
+                date_now.month,
+                date_now.day,
+                date_now.hour,
+                date_now.minute,
+                date_now.second)
+
+        local("tar -csvf {} /web_static/)".format(archive_path))
+        return archive_path
+    except Exception:
+        return None
