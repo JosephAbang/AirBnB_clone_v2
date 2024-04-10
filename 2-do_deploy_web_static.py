1c191099851ec6da0322717c6bb3d4e6904d15d0
JosephAbang <abangjosef@gmail.com>
2024-04-10 14:26:49 +0100

    Fix pycodestyle error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -38,9 +38,11 @@ def do_deploy(archive_path):
         archived_file_tmp = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file_tmp, newest_version))
+        run("sudo tar -xzf {} -C {}/".format
+            (archived_file_tmp, newest_version))
         run("sudo rm {}".format(archived_file_tmp))
-        run("sudo mv {}/web_static/* {}".format(newest_version,newest_version))
+        run("sudo mv {}/web_static/* {}".format
+            (newest_version, newest_version))
         run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(newest_version))
