ba4f0917f7b3c9d7d869b39c48276c0afbbff961
JosephAbang <abangjosef@gmail.com>
2024-04-23 21:55:09 +0100

    Display string through route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    Script starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_hbnb():
+    """display 'Hello HBNB'"""
+    return "<p>Hello HBNB!</p>"
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
