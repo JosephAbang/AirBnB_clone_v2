e248df6fca369bf0f824636234d8f4f9d7fe5f54
JosephAbang <abangjosef@gmail.com>
2024-04-23 22:17:05 +0100

    Display concatenated text via route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+    Script starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """display 'Hello HBNB'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """display string 'hbnb'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """display text concatenation"""
+    return 'C' + text.replace('_',' ')
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
