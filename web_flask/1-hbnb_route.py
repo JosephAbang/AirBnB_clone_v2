a75b4cb5a300fe251905256cd5cd88e7920d9473
JosephAbang <abangjosef@gmail.com>
2024-04-23 22:03:26 +0100

    Display string 'hbnb'

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+    Script starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """display 'Hello HBNB'"""
+    return "Hello HBNB!"
+
+
+def hbnb():
+    """display string 'hbnb'"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
